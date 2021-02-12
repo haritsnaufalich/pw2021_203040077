2d76deea42e54b5c61aeeb9ea99a12e2aa0c1ad8
haritsnaufalich <haritsnaufalich@outlook.com>
2021-02-12 13:49:38 +0700

    Menyelesaikan Video Petemuan 2

---
+++ pertemuan2/index.php
@@ -0,0 +1,67 @@
+<?php
+
+/*
+Harits Naufal Ichsan
+203040077
+https://github.com/haritsnaufalich/pw2021_203040077
+Pertemuan 2 - 12 Februari 2021
+Mempelajari Sintaks PHP
+*/
+
+/*
+Pertemuan 2 - PHP Dasar
+Sintaks PHP
+
+Standard Output
+echo, print
+print_r
+var_dump
+
+Penulisan sintaks PHP
+1. PHP di dalam HTML
+2. HTML di dalam PHP
+
+Variabel dan Tipe Data
+Variabel
+Tidak boleh diawali dengan angka, tapi boleh mengandung angka
+$nama = "Harits Naufal";
+echo "Halo, nama saya $nama";
+
+Operator
+Aritmatika
++ - * / %
+$x = 10;
+$y = 20;
+echo $x * $y;
+
+Penggabung String / Concatenation / concat
+.
+$nama_depan = "Harits";
+$nama_belakang = "Naufal";
+echo $nama_depan . " " . $nama_belakang;
+
+Assignment
+=, +=, -=, *=, /=, %=, .=
+$x = 1;
+$x -= 5;
+echo $x;
+$nama = "Harits";
+$nama .= " ";
+$nama .= "Naufal";
+echo $nama;
+
+Perbandingan
+<, >, <=, >=, ==, !=
+var_dump(1 == "1");
+
+Identitas
+===, !==
+var_dump(1 === "1")
+
+Logika
+&&, ||, !
+$x = 10;
+var_dump($x < 20 && $x % 2 == 0)
+*/
+
+?>
